3bb986d478c8b43bd278a5fa0087f3712ca18285
Nick DuBois <nick@caxy.com>
2019-08-21 19:09:51 -0500

    Music page work

--- src/routes/Music.js
+++ src/routes/Music.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
-import {MainPageContent} from '../CommonStyledComponents';
+import {MainPageContent, SmallLabel} from '../CommonStyledComponents';
 import {Col} from 'reactstrap';
 import {PageHeader} from '../components/pageHeader';
+import YoutubeModal from '../components/YoutubeModal';
+
+const Videos = styled.div`
+text-align:center;
+`;
+
+const VideoContainer = styled.div`
+  text-align:center;
+  display:inline-block;
+  margin:25px;  
+`;
+const VideoHeader = styled.div`
+  color:white;
+`;
 
 export default class Music extends React.Component {
 
@@ -12,6 +26,26 @@ export default class Music extends React.Component {
       <MainPageContent>
         <Col>
           <PageHeader SectionHeader="Music" />
+          <Videos>
+          <VideoContainer>
+            <SmallLabel>
+              Offical Demo
+            </SmallLabel>
+            <YoutubeModal VideoId="kCB_7-79Kbo"/>
+          </VideoContainer>
+          <VideoContainer>
+            <SmallLabel>
+              Rosalie
+            </SmallLabel>
+            <YoutubeModal VideoId="DVV6i5sSXq0"/>
+          </VideoContainer>
+          <VideoContainer>
+            <SmallLabel>
+            Temping Fate
+            </SmallLabel>
+            <YoutubeModal VideoId="z3inKsQkxMw"/>
+          </VideoContainer>
+          </Videos>
         </Col>
       </MainPageContent>
     );
